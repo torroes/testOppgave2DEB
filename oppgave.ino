05ad3579de170366c6d099e531f124ed622ed98c
torroes <torroes@hedmark.org>
2021-10-27 12:38:23 +0200

    ja

---
+++ oppgave.ino
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------
+gjør endringer i koden 
+a) Lag en kode som gjør slik at hvis en pushbutton er trykket inn vil en lysdiode lyse.
+    og hvis knappen ikke er trykket vil en annen lysdiode lyse.
+b) Endre koden slik at når du trykker knapp en vil du starte en funksjon som gjør at lysdiode
+    1. blinke 3 ganger med 1 sekund på og 1 sekund avtid.
+c) Endre koden slik at du kan slik at det skrives ut i serial monitor tiden du holdt knappen inne.   
+-----------------------------------------------------------------------------------*/
+
+
+void setup() 
+    {
+        Serial.begin(9600);
+       
+    }
+
+
+
+ 
+void loop() 
+    {
+        
+    }
